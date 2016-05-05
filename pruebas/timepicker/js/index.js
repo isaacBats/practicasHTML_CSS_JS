e91ac09eecf8ec3bc146d0ad7808b9840e0df281
isaac daniel batista <isaac@Mac-mini-de-Daniel-Bautista.local>
2016-05-05 13:55:34 -0500

    Reacomodando la carpeta de ejemplos de HTML CSS y JS

---
+++ pruebas/timepicker/js/index.js
@@ -0,0 +1,34 @@
+$(function () {
+    $('#datetimepicker3').datetimepicker({
+        format: 'LT'
+    });
+});
+
+$(function() {
+    $('#timepicker3').datetimepicker({
+      pickDate: false
+    });
+});
+
+/* Cambia el lenguaje del datepicker a español */
+$.datepicker.regional['es'] = {
+   closeText: 'Cerrar',
+   prevText: '<Ant',
+   nextText: 'Sig>',
+   currentText: 'Hoy',
+   monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+   monthNamesShort: ['Ene','Feb','Mar','Abr', 'May','Jun','Jul','Ago','Sep', 'Oct','Nov','Dic'],
+   dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+   dayNamesShort: ['Dom','Lun','Mar','Mié','Juv','Vie','Sáb'],
+   dayNamesMin: ['Do','Lu','Ma','Mi','Ju','Vi','Sá'],
+   weekHeader: 'Sm',
+   dateFormat: 'dd/mm/yy',
+   firstDay: 1,
+   isRTL: false,
+   showMonthAfterYear: false,
+   yearSuffix: ''
+};
+
+$.datepicker.setDefaults($.datepicker.regional['es']);
+
+
